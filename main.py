4c310587e033ae6c80597de62dc5ae80406507fd
Asadullah Azizi <habibya1401@gmail.com>
2026-07-19 17:29:48 +0430

    Explicitly bind host to 0.0.0.0 and dynamic port for Render

--- main.py
+++ main.py
@@ -222,5 +222,6 @@ def delete_post(post_id):
 
 
 if __name__ == "__main__":
-
-    app.run(debug=True)
+    # Get port from environment variable, default to 5000 if not found locally
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
